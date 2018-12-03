fc44eae102e6fc68bc8034ed921846fd4feeb2d2
Your Name Here <youremail@ccri.com>
2018-12-03 14:10:06 -0500

    Character Array
    Split the input letters into characters.

--- 2_1.rb
+++ 2_1.rb
@@ -3,6 +3,8 @@
 # Some things we need to start
 
 input_array = [] # An array to hold our numbers that we're about to read in
+ID2 = 0 # How many entries have the same letter twice?
+ID3 = 0 # How many entries have the same letter three times?
 
 # Get the input file
 
@@ -11,7 +13,11 @@ f = File.open("day_2_test.txt")
 # Read each line of the file and put its value into the array
 
 while line = f.gets do
-	input_array << line
+	input_array << line.to_s
 end
 
-puts input_array
+input_array.each do |x|
+
+	letters = x.chars
+	
+end

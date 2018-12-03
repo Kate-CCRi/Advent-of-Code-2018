8fd7813b08b8c50273e8403c3bf20a2aacc408dd
Your Name Here <youremail@ccri.com>
2018-12-03 15:59:50 -0500

    So much sorting!
    Sort each item in the input by character, then sort the overall array.
    (Such that aabbcc now comes before aabbcd)

--- 2_2.rb
+++ 2_2.rb
@@ -2,10 +2,8 @@
 
 # Some things we need to start
 
-input_array = [] # An array to hold our numbers that we're about to read in
-two_same = 0 # How many entries have the same letter twice?
-three_same = 0 # How many entries have the same letter three times?
-
+input = [] # An array to hold our numbers that we're about to read in
+sorted = []
 
 # Get the input file
 
@@ -14,5 +12,17 @@ f = File.open("day_2_test_2.txt")
 # Read each line of the file and put its value into the array
 
 while line = f.gets do
-	input_array << line
+	input << line
 end	
+
+# Sort the array into a new array
+sorted_bystring = input.sort
+
+sorted_bystring.each do |x|
+	
+	x.delete!("\n") # Remove the newline character from the string
+	letters = x.chars.sort # Create a new array out of the characters remaining in the string
+	sorted_byletter << letters
+	sorted << sorted_byletter.sort
+end
+

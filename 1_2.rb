3dd9cf5df161866dd4e649d9496ccccde944805c
Your Name Here <youremail@ccri.com>
2018-12-03 09:44:56 -0500

    First pass at a solution for Day 1, Problem 2

---
+++ 1_2.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+# Some things we need to start
+
+current_num = 0 # A variable to hold our current number
+num_array = [] # An array to hold our numbers that we're about to read in
+comp_array = [] # An array to hold the calculated numbers for comparison
+
+# Get the input file
+
+f = File.open("day_1_input.txt")
+
+# Read each line of the file and put its value into the array
+
+while line = f.gets do
+
+	input_num = line.to_i # Convert the read in strings into integers
+	num_array << input_num # Push that number to the array
+	
+end
+
+# Calculate the value of the number after each operation specified in the array
+
+num_array.each {|x| 
+
+	current_num = current_num + x
+	comp_array << current_num}
+
+# Find the first duplicate
+
+comp_array.find { |each| (h[each] += 1) == 2}

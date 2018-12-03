981daab405ae8332a12e723d251105320510d047
Your Name Here <youremail@ccri.com>
2018-12-03 16:43:36 -0500

    Each cons
    This is cute — work on consecutive items from an array! (Doesn’t wrap.
    Also not the right thing to do.)

--- 2_2.rb
+++ 2_2.rb
@@ -4,6 +4,8 @@
 
 input = [] # An array to hold our lines that we're about to read in
 sorted = []
+lcount = 0
+ldouble = 0
 
 # Get the input file
 
@@ -15,6 +17,7 @@ while line = f.gets do
 	input << line
 end	
 
+# Sort each item in the input list by letter
 input.each do |x|
 	
 	letters = []
@@ -23,11 +26,42 @@ input.each do |x|
 	
 	letters = x.chars.sort # Create a new array out of the characters remaining in the string and sort them
 	sorted << letters
+	
+	lcount = letters.count
+	ldouble = lcount * 2
 		
 end
+
+
  
+# Sort the overall list of things - this produces an array of arrays
 sorted.sort
 
-puts sorted
+# This compares each successive pair of items - not quite what was wanted.
+sorted.each_cons(2) do |one, two|
+
+	unique = [one, two].flatten.uniq
+	
+	if unique.length == lcount
+		puts "#{one} and #{two} are the same."
+	elsif unique.length == ldouble
+		puts "#{one} and #{two} are completely different"
+	end
+
+end
+
+
+=begin
+Some pseudocode
+
+for each array currently in sorted
+	pull out each other array in sorted
+	compare Array 1 to Arrays 2..n placewise
+	if only one place is different, return the values from each array
+	otherwise continue
+end
+=end
+
+
 
 

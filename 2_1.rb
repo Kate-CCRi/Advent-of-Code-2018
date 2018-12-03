dc729c4fa42828c26af31103d920a5cf9dc3041f
Your Name Here <youremail@ccri.com>
2018-12-03 14:44:33 -0500

    First try
    Lines 6-7: don’t use numbers in variable names, it confuses things.
    Line 16: Don’t need to .to_s this
    Line 19: Print the array to make sure the file got read right (testing
    line, will remove)
    Line 23: Remove newline characters if they exist.
    Lines 26 forward: New code.

--- 2_1.rb
+++ 2_1.rb
@@ -3,8 +3,8 @@
 # Some things we need to start
 
 input_array = [] # An array to hold our numbers that we're about to read in
-ID2 = 0 # How many entries have the same letter twice?
-ID3 = 0 # How many entries have the same letter three times?
+two_same = 0 # How many entries have the same letter twice?
+three_same = 0 # How many entries have the same letter three times?
 
 # Get the input file
 
@@ -13,11 +13,40 @@ f = File.open("day_2_test.txt")
 # Read each line of the file and put its value into the array
 
 while line = f.gets do
-	input_array << line.to_s
+	input_array << line
 end
+
+puts input_array	
 
 input_array.each do |x|
+	
+	x.delete!("\n")
 
 	letters = x.chars
+	l_uniq = letters.uniq
+	puts "The original array is #{letters}."
+	puts "The uniqued array is #{l_uniq}."
 	
+	l_uniq.each do |y|
+		puts y
+		if letters.count(y) == 2
+			doubles = 1
+		elsif letters.count(y) == 3
+			triples = 1
+		else
+		end		
+		if doubles == 1
+			two_same += 1
+			puts "There are now #{two_same} things with 2 letters the same."
+		elsif triples == 1
+			three_same += 1
+			puts "There are now #{three_same} items with 3 letters the same."
+		end
+	end
 end
+
+checksum = two_same * three_same
+
+puts "There are #{two_same} entries with 2 letters the same."
+puts "There are #{three_same} entries with 3 letters the same."
+puts "Your checksum is #{checksum}."

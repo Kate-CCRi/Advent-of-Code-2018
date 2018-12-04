48a8edab5d677555e6c5295107b224974309dbac
Your Name Here <youremail@ccri.com>
2018-12-04 13:26:48 -0500

    Grid and coordinates
    This sets up a 1000 x 1000 2D array called “grid” and then pulls the
    starting coordinate and dimensions out of the input file and assigns
    those to two variables.

--- 2018-03/3_1.rb
+++ 2018-03/3_1.rb
@@ -13,3 +13,17 @@ f = File.open("day_3_input.txt")
 while line = f.gets do
 	input << line
 end	
+
+# Instantiate a new 2D array of 1000 arrays with 1000 '0's in each
+grid = Array.new(1000) {Array.new(1000,0)}
+
+# For each item in the input (a "claim")
+input.each do |claim|
+
+=begin
+This line splits the claim into two pieces twice. The command is evaluated left to right, so it gets the claim and splits it on the @, which splits the original line into the claim ID on one side, and the starting coordinate and the dimensions on the other. Then it says "take the last part of that split" (so just the coordinate and dimensions) and split *that* on ":", and assign the two values returned to the two variables.
+=end
+
+	coord, dim = claim.split("@").last.split(":")
+
+end

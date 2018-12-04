156425b409d58690c237b160efdec06af2417c82
Kate <ksecor@ccri.com>
2018-12-04 13:54:41 -0500

    Main Development
    This sets up our coordinates and attempts to put a 1 in the grid at
    each point, then returns the number of grid spaces that have been
    incremented more than once.
    
    This doesn’t work because it thinks ‘w’ is a String rather than an Int.

--- 2018-03/3_1.rb
+++ 2018-03/3_1.rb
@@ -26,4 +26,29 @@ This line splits the claim into two pieces twice. The command is evaluated left 
 
 	coord, dim = claim.split("@").last.split(":")
 
+	# Pull whitespaces out
+	coord = coord.delete(' ')
+	dim = dim.delete(' ')
+	
+	# Split the coordinates on "," and assign the values to x and y
+	x, y = coord.split(",")
+	
+	# Split the dimensions on "x" and assign the values to w and h
+	w, h = dim.split("x")
+	
+=begin
+This block says:
+	As many times as you have entries in w
+		As many times as you have entries in h
+			Go to the grid coordinates for that square and put a 1 in it
+=end	
+	
+	w.times do |x1|
+    	h.times do |y1|
+     		grid[x + x1][y + y1] += 1
+     	end
+    end
 end
+
+# Take the grid array, flatten it, pick all cells where the number in them is greater than 1, and return that count.
+puts grid.flatten.select { |cell| cell > 1 }.count

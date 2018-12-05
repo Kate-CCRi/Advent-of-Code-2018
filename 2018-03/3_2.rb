f14ff81f76153ccf672cf2997dd0f2d7a8e25b15
Kate <ksecor@ccri.com>
2018-12-05 10:05:12 -0500

    Success!
    So yes, sets were the answer.
    
    See comments for explanation of what it’s doing.

--- 2018-03/3_2.rb
+++ 2018-03/3_2.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'set'
+
 # Some things we need to start
 
 # An array to hold our lines that we're about to read in
@@ -14,12 +16,21 @@ while line = f.gets do
 end
 
 
-# Creates a new nested array: 1000 rows containing new arrays, each of which contains 1000 '0's.
+=begin
+This creates a new nested array -- 1000 rows containing new arrays, each of which contains 1000 new sets, which will be used to hold the IDs we've seen as we go through the grid.
 
-grid = Array.new(1000) {Array.new(1000, 0)}
+So our array now looks like this - using {} for a set rather than for a hash:
+[
+	[{},{},{},...{}]
+	[{},{},{},...{}]
+	...
+]
+=end
 
-# A new array to hold my potential candidate IDs.
-candidates = []
+grid = Array.new(1000) {Array.new(1000) {Set.new}}
+
+# A new set to hold my potential candidate IDs.
+candidates = Set.new
 
 # For each item in the input (a "claim")
 input.each do |claim|
@@ -30,7 +41,7 @@ This splits the claim into three pieces and then splits the second one again. Th
 It also uses "chomp" and "delete" to do some cleanup on the things it's found in during the split. Note that you *cannot* cast 'coord' and 'dim' to integers here because you need to split them further in the next step.
 =end
 	
-	id = claim.split("@").first.chomp.delete("#")
+	id = claim.split("@").first.chomp.delete("#").delete(" ")
 	
 	coord, dim = claim.delete(' ').chomp.split("@").last.split(":")
 	
@@ -42,32 +53,34 @@ It also uses "chomp" and "delete" to do some cleanup on the things it's found in
 	
 	w, h = dim.split("x").map(&:to_i)
 
-	# Put the ID of the claim you're looking at into the "candidates" array
+	# Put the ID of the claim you're looking at into the "candidates" set	
 
-	candidates << id
-	puts candidates
-	
+	candidates.add(id)
+
 =begin
-This block says:
-	As many times as you have entries in w
-		As many times as you have entries in h
-			Push the ID that made you go to that coordinate into the set at that spot on the grid.
+Go through the dimensions described by each claim. 
+If the set in that cell has anything in it:
+	- delete the current candidate from the candidate set
+	- delete any IDs contained within the cell from the candidate set
+Otherwise, add the current ID to the cell you're looking at.
 =end	
 	
 	w.times do |x1|
     	h.times do |y1|
-     		grid[x + x1][y + y1] +=1
-     		if grid[x + x1][y + y1] > 1
-				candidates.pop
-			end
-     	end
-    end
+     		cell = grid[x + x1][y + y1]
+     		
+     		if cell.any?
+     			candidates.delete(id)
+     			cell.each do |c|
+     				candidates.delete(c)
+     			end
+     		else
+     			cell.add(id)
+     		end
+     	end	
+     end			
  end
     
-=begin
-Now you have a grid where each grid cell contains one copy of each claim ID that made you visit that cell.
+# Output a human-readable version of the set with the remaining claim ID in it.
 
-So. Iterate over the grid by claim *again*, and for every cell that has more than one thing in it, remove that claim from your list of possibilities.
-=end 
-
-puts candidates.count	
+puts candidates.inspect

4286bdaea7df9ec7111691905aac93eff256a081
Kate <ksecor@ccri.com>
2018-12-05 10:41:41 -0500

    Input fix
    I couldn’t figure out how to get the newline out of the input array, so
    I stole this idea from gchan about just reading the file and then
    splitting it on the newline character, which automatically makes it go
    away.

--- Day 04/4_1.rb
+++ Day 04/4_1.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/ruby
 
-# Some things we need to start
-
-# An array to hold our lines that we're about to read in
-input = []
-
 # Get the input file
-f = File.open("day_4_test.txt")
+input = File.read("day_4_test.txt")
 
-# Read each line of the file and put its value into the array
-while line = f.gets do
-	input << line
-end
+# Make a new array called "logs" that has the text from "input" split on the newline character (which conveniently makes that character go away)
+logs = input.split("\n").sort
+
+# Note that the .inspect tag forces it to show you the actual structure of your array, not just the contents.
+puts logs.inspect
+
+=begin
+Sort the list into time order. Note that not all times start with 00.
+Create a nested array like so:
+[
+	Day
+	[
+		Guard
+		[
+		0..59
+		]
+	]
+]
+
+times = Array.new
+	
+If the first item in the line after the time is "Guard", go to that line in the array.
+Then:
+	If the first item in the next line after the time is "falls", put the minutes in that timestamp into a "marked" array.
+	If the first item in the line after the time is "wakes", put (the minute - 1) in that timestamp into the "marked" array AND
+	Mark the cells in the range of the "marked" array as "asleep".
+=end

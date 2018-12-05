cea4aa737466c4994adf29f6a0fee3cb2697e1dd
Kate <ksecor@ccri.com>
2018-12-05 10:50:47 -0500

    Set up variables
    Set up the variables I want to work with from each record with the
    right data.

--- Day 04/4_1.rb
+++ Day 04/4_1.rb
@@ -3,14 +3,11 @@
 # Get the input file
 input = File.read("day_4_test.txt")
 
-# Make a new array called "logs" that has the text from "input" split on the newline character (which conveniently makes that character go away)
+# Make a new array called "logs" that has the text from "input" split on the newline character (which conveniently makes that character go away). This also automatically sorts it into the proper day/time order.
+
 logs = input.split("\n").sort
 
-# Note that the .inspect tag forces it to show you the actual structure of your array, not just the contents.
-puts logs.inspect
-
 =begin
-Sort the list into time order. Note that not all times start with 00.
 Create a nested array like so:
 [
 	Day
@@ -21,8 +18,6 @@ Create a nested array like so:
 		]
 	]
 ]
-
-times = Array.new
 	
 If the first item in the line after the time is "Guard", go to that line in the array.
 Then:
@@ -30,3 +25,19 @@ Then:
 	If the first item in the line after the time is "wakes", put (the minute - 1) in that timestamp into the "marked" array AND
 	Mark the cells in the range of the "marked" array as "asleep".
 =end
+
+logs.each do |record|
+	guard_logs = record.delete("[").delete("]").split
+	
+	guard_logs.each do |line|
+		date = line[0..9]
+		time = line[11..15]
+		action = line[17]
+		number = line[19..21]
+		
+		puts date
+		puts time
+		puts action
+		puts number
+	end
+end

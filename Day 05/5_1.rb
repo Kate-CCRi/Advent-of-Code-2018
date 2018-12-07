9d30ed4dd5c2d9fe9a622d395a2aa9d2d57814d7
Kate <ksecor@ccri.com>
2018-12-07 09:59:55 -0500

    Add some reporting and testing
    This works, but doesn’t return the right answer.

--- Day 05/5_1.rb
+++ Day 05/5_1.rb
@@ -5,16 +5,28 @@ input = File.read("day_5_test.txt")
 
 # Set up an array to read the input by character
 
+test_answer = ["d", "a", "b", "C", "B", "A", "c", "a", "D", "A"]
 polymer = input.chars
 newpolymer = []
 
-# Iterate over the array in groups of two
-polymer.each_slice(2) do |x, y|
-	
-	unless x == y.upcase or x == y.downcase
-		newpolymer << x
-		newpolymer << y
+
+# I need to wrap this in a new iterator so that I can set polymer to the cleaned version and go over it again
+
+
+	# Iterate over the array by consecutive pairs: (1,2) then (2, 3) etc.
+	polymer.each_cons(2) do |x, y|
+
+		unless x == y.upcase or x == y.downcase
+		
+			newpolymer << x
+
+		end
 	end
+	
+if newpolymer == test_answer
+	puts "This is correct."
+else
+	puts test_answer.inspect
+	puts newpolymer.inspect
 end
 
-puts newpolymer

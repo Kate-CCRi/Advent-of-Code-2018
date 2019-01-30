393e09262713e3680bda5d486c87041d46dc6ad5
Kate <ksecor@ccri.com>
2019-01-30 17:06:32 -0500

    It works!
    This is kind of brute force, because you have to re-run all of day 1 to
    make it work, but it does work.

--- Day 05/5_2.rb
+++ Day 05/5_2.rb
@@ -1,49 +1,54 @@
 #!/usr/bin/ruby
 
 # Get the input file
-input = File.read("day_5_test.txt")
+input = File.read("day_5_input.txt")
 
-# Set up an array to read the input by character
+# Set up an array to read the input by character and a variable that has its length
 polymer = input.chars
 shortest = polymer.length
 
+# Push the entire alphabet into an array
 alphabet = ('a'..'z').to_a
 
+
+# For each letter in the alphabet array
 alphabet.each do |letter|
 
+# Make a copy of the original polymer
 	working = polymer.map(&:clone)
-	
+
+# Make a new array that deletes the letter and its uppercase version from the polymer	
 	working2 = working.delete_if{ |item| item == letter or item == letter.swapcase}
 	
-	puts polymer.inspect
-	puts working.inspect
-	puts working2.inspect
 
-end
+# This is the Day 1 reaction code
+	i = 0
 
-
-		
-		
-
-
-=begin
-i = 0
-
-# Start looping through
-loop do
-	# Stop looping if you've reached the second-to-last character (nothing left to compare)
-	break if i == polymer.size - 1
-	# Checks to see if the first thing is an uppercase or lowercase of the second thing
-	if polymer[i].swapcase == polymer[i + 1]
-		# If yes, delete the two things you just compared
-		2.times {polymer.delete_at(i)}
-		# Unless you were at the first character, back up one step
-		unless i == 0
-			i -= 1
+	# Start looping through
+	loop do
+		# Stop looping if you've reached the second-to-last character (nothing left to compare)
+		break if i == working2.size - 1
+		# Checks to see if the first thing is an uppercase or lowercase of the second thing
+		if working2[i].swapcase == working2[i + 1]
+			# If yes, delete the two things you just compared
+			2.times {working2.delete_at(i)}
+			# Unless you were at the first character, back up one step
+			unless i == 0
+				i -= 1
+			end
+		# If the comparison is false, move on to the next set
+		else
+			i += 1
 		end
-	# If the comparison is false, move on to the next set
-	else
-		i += 1
 	end
+
+# If the length of the reacted redacted polymer is less than the current shortest length, reset the current shortest length	
+	if working2.length < shortest
+		shortest = working2.length
+	end
+
 end
-=end
+
+# Return the shortest length seen
+puts shortest
+

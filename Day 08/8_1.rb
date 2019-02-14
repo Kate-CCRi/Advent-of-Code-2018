2731d4b4d5e710f4e4d91213a573067d87d00a7e
Kate <ksecor@ccri.com>
2019-02-14 14:36:02 -0500

    Not wrong, but incomplete
    * Commented out the Tree implementation as maybe unnecessary?
    * Started a nested array setup
    * This works as far as it goes, but won't work past the first "no children" item
    * Put in a suggestion on how to re-write the iterator

--- Day 08/8_1.rb
+++ Day 08/8_1.rb
@@ -4,16 +4,20 @@ data_index = []
 
 char_file = File.open('day_8_input.txt')
 char_file.each(" ") do |char|
-		data_index << char
+		data_index << char.to_i
 end
+
+=begin
 # Beginning of a Tree implementation
+
 module Tree
 
 	class Node
 
-		attr_accessor :children, :content
+		attr_accessor :children, :content, :name
 	
-		def initialize(content)
+		def initialize(name, content)
+			@name = name
 			@children = []
 			@content = content
 		end
@@ -27,10 +31,25 @@ module Tree
 			puts "#{node.content}: #{children}"
 		end
 		
+		def traverse_subtree(&block)
+			node_queue = [self]
+			until node_queue.empty?
+				node = node_queue.shift
+				yield node
+				node_queue = node.children.concat(node_queue)
+			end
+		end
+		
+		def dependency_list(node)
+			node.children.flat_map do |child|
+				[child.content] + dependency_list(child)
+			end
+		end
+		
 	end
 	
 end
-
+=end
 
 =begin
 until data_index.length == 0
@@ -73,4 +92,60 @@ end
 puts sum
 
 =end
+
+# The idea is to set up an array [# children, # metadata, [child info], metadata entries]
+data_array = []
+
+data_array << data_index[0]
+data_array << data_index[1]
+
+
+	holder = []
+	index_count = 2
+
+# This sort of does the right thing, but I think maybe I need to make it 'N' times do, where N is the number of children each thing has?
+	
+5.times do
+
+	unless data_index[index_count] == 0
+	
+		holder.clear
+		
+		holder << data_index[index_count]
+		holder << data_index[index_count + 1]
+
+		data_array << [holder[0], holder[1]]
+		
+		index_count +=2
+		
+	else
+		holder.clear
+	
+		holder << data_index[index_count]
+		holder << data_index[index_count + 1]
+
+		data_array << [holder[0], holder[1]]
+		
+		counter = 2
+		holder.clear
+		
+		data_index[index_count + 1].times do 
+			
+			holder << data_index[index_count + counter]
+			
+			counter += 1
+		end
+		
+		holder.each do |item|
+			data_array.last << item
+		end
+		
+	end
+end
+
+
+
+
+p data_array
+
 	

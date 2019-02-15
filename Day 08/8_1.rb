88a3ef6803bff20f484dc61088f509826f9ed53a
Kate <ksecor@ccri.com>
2019-02-15 16:32:08 -0500

    Almost there
    * Added some stuff that will properly insert the metadata into the right grouping
    * Need to update the iterator so it keeps going

--- Day 08/8_1.rb
+++ Day 08/8_1.rb
@@ -7,92 +7,106 @@ char_file.each(" ") do |char|
 		info << char.to_i
 end
 
+# Set up some counters and holders
+
+holder = []
+index_count = 2
+working_on = []
+level = 0
+children = []
 total_sum = 0
 
 # The idea is to set up an array [# children, # metadata, [child info], metadata entries]
 data_array = []
 
 # Push the first two pieces of data into the data array
-data_array << info[0]
-data_array << info[1]
+data_array << [info[0], info[1]]
+children << info[1]
 
-# Set up some counters and holders
 
-holder = []
-index_count = 2
-level = 100
-working_on = []
 
 # This sort of does the right thing, but I think maybe I need to make it 'N' times do, where N is the number of children each thing has?
 	
-until level == 0
+# until <TODO: figure out the iterator>
 
-	# All of these have children because the first number isn't 0
+8.times do
+
+	# Clear the holder
+	holder.clear
+		
+	# Put the next two pieces of information into the holder
+	holder << info[index_count]
+	holder << info[index_count + 1]
 	
-		if level == 100
-			level = 0
-		end
-		
-		# Clear the holder
-		holder.clear
-		
-		# Put the next two pieces of information into the holder
-		holder << info[index_count]
-		holder << info[index_count + 1]
+	# Add the child count and metadata count to the data array and the children count of this item to the child_count array
+	data_array << [holder[0], holder[1]]
+	children << holder[1]
+	
+	puts "I just added #{holder.inspect}"
 
-		# Push this information into the data array as a sub-array
-		data_array << [holder[0], holder[1]]
-		
-		
-	if info[index_count] != 0
+	unless holder[0] == 0
+	
 		# Add 2 to the index count so you can get to the next set of children/metadata count info
 		index_count +=2
 		
 		# Add 1 to the level because you're one level down now
 		level += 1
-		
-	else
-		counter = 0
-		index_count = index_count + 2
-		
-		info[index_count - 1].times do 
 	
-			total_sum += info[index_count + counter]
-			
+		puts "and the level is #{level}"
+		
+	else 
+		
+		# Set up a counter to increment the index count
+		counter = 2
+		
+		# The number of times you want to add the metadata to the item
+		repeat = holder[1]
+		
+		# Clear the holder so you can reuse it
+		holder.clear
+		
+		# As many times as the item you just added has metadata items, add those items to the holder and then increment the counter
+		repeat.times do
+			holder << info[index_count + counter]
 			counter += 1
-			
 		end
 		
-		
-		
-		index_count = index_count + counter
-	
-		working_on = data_array.pop
-	
-		if working_on[0] == working_on.length - 2
-		
-			working_on[1].times do
-				total_sum += info[index_count]
-				index_count += 1
-			end
-
-		data_array.last << [working_on[0], working_on[1]]
-		level -= 1
-		
-		working_on.clear
-		
+		# Put each item in the holder into the last item in the data array
+		holder.each do |item|
+			data_array.last << item
 		end
+		
+		# Update the index count
+		index_count = index_count + counter	
+		
+		# Mark the index in children that corresponds with the set of data you just processed as "done"
+		children.delete_at(-1)
+		children << "done"
+		
+		level = level - 1
 	end
 	
-
-
+	if children[-1] == "done"
+	
+		children_index = children.find_index(children[-2])
 		
+		children[children_index].times do
+		
+			data_array[children_index] << info[index_count]
+		
+			index_count += 1
+		end
+		
+		children.delete_at(children_index)
+		children << "done"
+		
+	end
 end
 
-puts total_sum
-puts level
+puts "You are at level #{level}"
 
 p data_array
 p working_on
+p children
 
 	

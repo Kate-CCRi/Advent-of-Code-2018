00b38ecee476c8c9136a4621f7d1d539843fba0c
Kate <ksecor@ccri.com>
2019-02-15 08:50:52 -0500

    Mid-stream adding
    * Deleted code up at the top for old tries
    * Changed the name of data_index to info because I was getting confused by the two name arrays
    * Added some comments in the first block of code
    * Changed the 0 case code to include adding the totals as I go instead of thinking about working backwards with the level variable

--- Day 08/8_1.rb
+++ Day 08/8_1.rb
@@ -1,168 +1,99 @@
 #/usr/bin/ruby!
 
-data_index = []
+info = []
 
 char_file = File.open('day_8_input.txt')
 char_file.each(" ") do |char|
-		data_index << char.to_i
+		info << char.to_i
 end
 
-=begin
-# Beginning of a Tree implementation
-
-module Tree
-
-	class Node
-
-		attr_accessor :children, :content, :name
-	
-		def initialize(name, content)
-			@name = name
-			@children = []
-			@content = content
-		end
-	
-		def add_child(child)
-			@children << child
-		end
-		
-		def print_subtree(node)
-			children = node.children.map { |child| child.content}
-			puts "#{node.content}: #{children}"
-		end
-		
-		def traverse_subtree(&block)
-			node_queue = [self]
-			until node_queue.empty?
-				node = node_queue.shift
-				yield node
-				node_queue = node.children.concat(node_queue)
-			end
-		end
-		
-		def dependency_list(node)
-			node.children.flat_map do |child|
-				[child.content] + dependency_list(child)
-			end
-		end
-		
-	end
-	
-end
-=end
-
-=begin
-until data_index.length == 0
-
-	puts "The data index length is #{data_index.length}"
-
-	child_count = data_index[0].to_i
-	metadata_count = data_index[1].to_i
-	
-	puts "This item has #{child_count} children"
-	puts "This item has #{metadata_count} pieces of metadata"
-	
-	if child_count == 0
-	
-		puts "The child count was 0"
-		
-		metadata_count.times do
-			puts "I am about to add #{data_index[2]} to #{sum}"
-			sum += data_index[2].to_i
-			puts "I am about to delete #{data_index[2]}"
-			data_index.delete_at(2)
-		end
-	
-	else
-		metadata_count.times do
-		
-			puts "I am about to add #{data_index[-1]} to #{sum}"
-		
-			sum += data_index.pop.to_i
-		end
-		
-	end
-	
-	2.times do
-		puts "I am about to remove #{data_index[0]}"
-		data_index.shift
-	end
-end
-
-puts sum
-
-=end
+total_sum = 0
 
 # The idea is to set up an array [# children, # metadata, [child info], metadata entries]
 data_array = []
 
-data_array << data_index[0]
-data_array << data_index[1]
+# Push the first two pieces of data into the data array
+data_array << info[0]
+data_array << info[1]
 
+# Set up some counters and holders
 
-	holder = []
-	index_count = 2
-	level = 1
+holder = []
+index_count = 2
+level = 1
 
 # This sort of does the right thing, but I think maybe I need to make it 'N' times do, where N is the number of children each thing has?
 	
-# data_index[0].times do
+# info[0].times do
 
-	until data_index[index_count] == 0
+	# All of these have children because the first number isn't 0
+	until info[index_count] == 0
 		
+		# Clear the holder
 		holder.clear
 		
-		holder << data_index[index_count]
-		holder << data_index[index_count + 1]
+		# Put the next two pieces of information into the holder
+		holder << info[index_count]
+		holder << info[index_count + 1]
 
+		# Push this information into the data array as a sub-array
 		data_array << [holder[0], holder[1]]
 		
+		# Add 2 to the index count so you can get to the next set of children/metadata count info
 		index_count +=2
+		
+		# Add 1 to the level because you're one level down now
 		level += 1
 		
 	end
 	
-	if data_index[index_count] == 0
+	
+	if info[index_count] == 0
 		holder.clear
 		
-		holder << data_index[index_count]
-		holder << data_index[index_count + 1]
+		holder << info[index_count]
+		holder << info[index_count + 1]
 		
 		holder2 = []
 		holder2 << [holder[0], holder[1]]
+
+		counter = 0
+		index_count = index_count + 2
 		
-		counter = 2
-		
-		
-		holder3 = []
-		
-		data_index[index_count + 1].times do 
-			index_count = index_count + counter
-			
-			
-			holder3 << data_index[index_count]
+		info[index_count - 1].times do 
+	
+			total_sum += info[index_count + counter]
 			
 			counter += 1
-			index_count +=1
-		end
-		
-		holder3.each do |item|
-			holder2 << item
+			
 		end
 		
 		data_array.last << holder2
 		
+		index_count = index_count + counter
+	
+		working_on = data_array.pop
+	
+		if working_on[0] == working_on.length - 2
+		
+			working_on[1].times do
+				total_sum += info[index_count]
+				index_count += 1
+			end
+
+			p working_on
+		
+		end
 	end
 	
-	working_on = data_array.pop
+
 
 		
 # end
 
-puts data_index[index_count]
+puts total_sum
 
 p data_array
-
-p data_array.last
+p working_on
 
 	

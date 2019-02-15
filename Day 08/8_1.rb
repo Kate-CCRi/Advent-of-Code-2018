95280bcfb33ec723b9001ce118cdfbb6eb607ac7
Kate <ksecor@ccri.com>
2019-02-15 10:27:31 -0500

    Add levels
    * Add the concept of going up/down a level when you do the sums
    
    This calculates fine for things with no children (I think) but doesn't do anything with the others, so the answer is wrong. (Which means it's working, it's only set up to do that right now -- I need to add the code to handle the "if it does have children" recursion case.)

--- Day 08/8_1.rb
+++ Day 08/8_1.rb
@@ -20,14 +20,18 @@ data_array << info[1]
 
 holder = []
 index_count = 2
-level = 1
+level = 100
+working_on = []
 
 # This sort of does the right thing, but I think maybe I need to make it 'N' times do, where N is the number of children each thing has?
 	
-# info[0].times do
+until level == 0
 
 	# All of these have children because the first number isn't 0
-	until info[index_count] == 0
+	
+		if level == 100
+			level = 0
+		end
 		
 		# Clear the holder
 		holder.clear
@@ -39,24 +43,15 @@ level = 1
 		# Push this information into the data array as a sub-array
 		data_array << [holder[0], holder[1]]
 		
+		
+	if info[index_count] != 0
 		# Add 2 to the index count so you can get to the next set of children/metadata count info
 		index_count +=2
 		
 		# Add 1 to the level because you're one level down now
 		level += 1
 		
-	end
-	
-	
-	if info[index_count] == 0
-		holder.clear
-		
-		holder << info[index_count]
-		holder << info[index_count + 1]
-		
-		holder2 = []
-		holder2 << [holder[0], holder[1]]
-
+	else
 		counter = 0
 		index_count = index_count + 2
 		
@@ -68,7 +63,7 @@ level = 1
 			
 		end
 		
-		data_array.last << holder2
+		
 		
 		index_count = index_count + counter
 	
@@ -81,7 +76,10 @@ level = 1
 				index_count += 1
 			end
 
-			p working_on
+		data_array.last << [working_on[0], working_on[1]]
+		level -= 1
+		
+		working_on.clear
 		
 		end
 	end
@@ -89,9 +87,10 @@ level = 1
 
 
 		
-# end
+end
 
 puts total_sum
+puts level
 
 p data_array
 p working_on
